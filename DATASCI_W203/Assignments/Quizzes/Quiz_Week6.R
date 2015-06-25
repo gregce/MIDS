cbc6ee3bd6eb25014e2311908d96f3c5d7dff869
gregce <gregce@gmail.com>
2015-06-24 22:11:42 -0700

    completion of week 6 quiz #2

--- DATASCI_W203/Assignments/Quizzes/Quiz_Week6.R
+++ DATASCI_W203/Assignments/Quizzes/Quiz_Week6.R
@@ -1,51 +1,37 @@
-prime.sieve <- function(number, first_prime=2) {
-  ##hold a vector of numbers to check in a variable  
-  ##start at 2 given the above note using the function's default argument
-  to_check <- c(first_prime:number) 
-  
-  ##initatilize primes variable vector to store results into  
-  primes <- NULL
-  count <- 0
-  
-  ##initatilize loop variable  
-  loop <- first_prime
-  while (loop*loop < number) {
-    primes <- c(primes, to_check[1])
-    ##remove multiples from to_check vector
-    to_check <- subset(to_check, !to_check %% loop == 0)
-    
-    ## update loop variable with next prime after multiples are removed
-  loop <- to_check[1]
-    count <- count+1
-  }
-  ##given the loop stops "prematurely", concatenate remaining primes from to_check vector with primes storage vector 
-  primes <- c(primes,to_check)
-  
-  ##can comment out these two print lines, for info only
-  print(paste("Loop executed ", count, " times"))
-  print(paste("Your number,", number, ", contains ", length(primes), " primes up to it and they are:"))
-  return(primes)
-}
-
-prime.sieve(1000)
-
 ##http://www.cyclismo.org/tutorial/R/pValues.html
 ##http://www.measuringu.com/zcalc.htm
 ##http://www.dummies.com/how-to/content/how-to-use-if133else-statements-in-r.html
 ##http://www.statmethods.net/management/controlstructures.html
+library(plyr)
 
-t.test <- function(data=c(), mu, pop.sd, two.tailed=TRUE, significance=.05) {
-  xbar <- mean(data)
-  n <- length(data)
-  z <- (xbar - mu) / (pop.sd /sqrt(n))
+testfun <- function(data=c(), mu, pop.sd, two.tailed=TRUE, significance=.05) {
+      ##computed sample mean
+      xbar <- mean(data)
+      ##computed sample size
+      n <- length(data) 
+      ##compute z score
+      z <- (xbar - mu) / (pop.sd /sqrt(n))
+     
+      #modify and compute pvalue depending on type of test required
+       if (two.tailed==TRUE) {
+      p <- 2 * pnorm(z, lower.tail = FALSE)
+      } else if (two.tailed==FALSE & xbar > mu) {
+      p <- pnorm((z), lower.tail = FALSE)
+      } else if (two.tailed==FALSE & xbar < mu) {
+      p <- pnorm((z))
+      }
+      
+      #create boolean value depending on 
+      if (p>significance) {
+      pass <- FALSE
+      } else {
+      pass <- TRUE
+      }
+    
+    df <- data.frame(p, pass)
+    colnames(df) <- c("pvalue","pass")
+    return(df)
+    ##return both the p value and pass boolean value
+    #return(list(output1=p,output2=pass))
  
-   if (two.tailed==TRUE) {
-  p <- 2*pnorm(-abs(z))
-  } else {
-  p <- pnorm(-abs(z))
-  }
-  
-  
-  return(list(p)
-  
 }
